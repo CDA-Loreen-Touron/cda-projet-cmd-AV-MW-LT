74bdce5441e43f2de9541ffa6c03e7ab4a3cdf7f
loreen12 <loreen.anaelle.touron@gmail.com>
2020-05-29 09:55:39 +0200

    Ajout Classe CD, supprime exit

--- cmd-project/src/models/Commande.java
+++ cmd-project/src/models/Commande.java
@@ -4,28 +4,17 @@ public abstract class Commande implements IExecutable {
 
 	private String nom;
 	
-<<<<<<< HEAD
+
 	public Commande(String pNom) {
 		this.nom=pNom;
 	}
-=======
-	private String nom;
 
-	public Commande (String pNom) {
-	this.nom=pNom;
-}
 	
 	
-	
-	
-	
->>>>>>> dev-loreen
 
 	public String getNom() {
 		return nom;
 	}
-public void executer() {
-	// TODO Auto-generated method stub
 	
-}
+
 }
