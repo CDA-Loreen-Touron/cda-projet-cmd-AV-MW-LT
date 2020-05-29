87b1f55e45354d9b8e6e6fdbc287ef90914b180b
aurelien-vannier <aurelien.vannier@hotmail.fr>
2020-05-29 15:27:02 +0200

    maj river

--- cmd-project/src/models/CommandeRiver.java
+++ cmd-project/src/models/CommandeRiver.java
@@ -1,50 +1,64 @@
 package models;
 
-import tools.EntreeClavier;
-
 public final class CommandeRiver extends Commande {
 
 	private String param;
-	
+
 	public CommandeRiver(String pNom) {
 		super(pNom);
-		this.param=param;
 
 	}
 
-	
 	@Override
 	public void executer() {
-		try {
-		
-			int a = EntreeClavier.lireInt();
-			int b=0;
-			if(a<1) {
-			
-			
-			while (a!=b ) {
-				if(a<b) {
-					a+=sumDigits(a);
 
-				}else {
-					b+=sumDigits(b);
+		try {
+			int index = 0;
+			String paramB = "";
+
+			for (int i = 0; i < param.length(); i++) {
+				if (param.charAt(i) == ' ') {
+					index = i;
+					break;
+				} else if (param.charAt(i) == '-') {
+					break;
+				}
+			}
+
+			int a = Integer.valueOf(param.substring(0, index).trim());
+
+			paramB = param.substring(index + 1, param.length()).trim();
+
+			for (int i = 0; i < paramB.length(); i++) {
+				if (paramB.charAt(i) == '-') {
+					System.out.println("Erreur de saisie");
+					break;
+				} else {
+
+					int b = Integer.valueOf(paramB);
+
+					while (a != b) {
+						if (a < b) {
+							a += sumDigits(a);
+
+						} else {
+							b += sumDigits(b);
+						}
+					}
 				}
 			}
 			System.out.println("Les rivières se croisent en " + a);
-			
-			}
-		
-			}catch (Exception e) {
-	System.out.println("Saisie incorrecte.");
+		} catch (Exception e) {
+			System.out.println("Saisie incorrecte.");
 		}
-}
-	
-	public static int sumDigits(int pNombre) {
-	    return pNombre == 0 ? 0 : pNombre % 10 + sumDigits(pNombre / 10);
 	}
 
+	public static int sumDigits(int pNombre) {
+		return pNombre == 0 ? 0 : pNombre % 10 + sumDigits(pNombre / 10);
+	}
 
 	public void setParam(String param) {
-		this.param = param;
+		this.param = param.trim();
+
 	}
 }
