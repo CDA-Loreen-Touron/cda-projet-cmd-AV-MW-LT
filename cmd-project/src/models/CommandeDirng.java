d599e0f1d00967b9dcd135cce43cd0233591c872
aurelien-vannier <aurelien.vannier@hotmail.fr>
2020-06-01 17:24:15 +0200

    maj dir et dirng

---
+++ cmd-project/src/models/CommandeDirng.java
@@ -0,0 +1,68 @@
+package models;
+
+import java.io.File;
+
+public final class CommandeDirng extends Commande{
+
+	
+	private String params = "";
+    public int filecount = 0;
+    public int dircount = 0;
+
+
+public CommandeDirng(String pNom) {
+	super(pNom);
+}
+
+
+@Override
+public void executer() {
+
+	  listDirectory(params);
+	  System.out.println("----------");
+	  System.out.println("Analyse de " + params );
+	  System.out.println(dircount + " dossiers");
+	  System.out.println(filecount + " fichiers");
+	  this.filecount=0;
+	  this.dircount=0;
+	}
+
+
+private void listDirectory(String dir) {
+    File file = new File(dir);
+    File[] files = file.listFiles();
+    if (files != null) {
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory() == true) {
+                System.out.println("Dossier: " + files[i].getAbsolutePath());
+                this.dircount++;
+            } else {
+                System.out.println("  Fichier: " + files[i].getName());
+                this.filecount++;
+            }
+        }
+    }
+}
+
+public String getParams() {
+	return params;
+}
+
+public  void setParams(String params) {
+	this.params = params;
+}
+
+
+public int getFilecount() {
+	return filecount;
+}
+
+public int getDircount() {
+	return dircount;
+}    
+
+
+
+}
+
+
