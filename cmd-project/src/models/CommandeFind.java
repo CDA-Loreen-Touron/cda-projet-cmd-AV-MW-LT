89088f20db1dde863d5e705539c8925679f6dad5
matthieu <matthieu_waroux59@hotmail.com>
2020-06-02 14:39:09 +0200

    corection

--- cmd-project/src/models/CommandeFind.java
+++ cmd-project/src/models/CommandeFind.java
@@ -19,14 +19,62 @@ public class CommandeFind extends Commande {
 		System.out.println(String.format("nombre de fichiers trouvés : %s", compteur));
 	}
 
+	private String refactorString3000(String pParams) {
+		boolean s = false;
+		boolean e = false;
+		String finale = "";
+		int finaleIndS = pParams.length();
+		int debutIndS = 0;
+		int finaleIndE = pParams.length();
+		int debutIndE = 0;
+
+		for (int i = 0; i < pParams.length(); i++) {
+			if (pParams.charAt(i) == '-') {
+				if (pParams.charAt(i + 1) == 's') {
+					if (!s) {
+						s = true;
+						debutIndS = i;
+						if (finaleIndE == pParams.length() && debutIndS != 0) {
+							finaleIndE = debutIndS;
+						}
+					} else {
+						finaleIndS = i;
+					}
+				}
+				if (pParams.charAt(i + 1) == 'e') {
+					if (!e ) {
+						e = true;
+						debutIndE = i;
+						if (finaleIndS == pParams.length() && debutIndE != 0) {
+							finaleIndS = debutIndE;
+						}
+					} else {
+
+						finaleIndE = i;
+					}
+				}
+
+			}
+
+		}
+		finale = pParams.substring(debutIndS, finaleIndS) + pParams.substring(debutIndE, finaleIndE);
+
+		return finale;
+	}
+
 	public void determineOption(String pParams) {
-		this.compteur=0;
+		this.compteur = 0;
 
 		if (pParams.contains("-starts") && pParams.contains("-ends")) {
+			refactorString3000(pParams);
 			option = 1;
+			for (int i = 0; i < pParams.length(); i++) {
+
+			}
+
 			int indice = pParams.indexOf('-');
-			if(pParams.charAt(indice+1)=='s') {
-				ordre =true;
+			if (pParams.charAt(indice + 1) == 's') {
+				ordre = true;
 			}
 			this.params = pParams.replace("-starts", "/").replace("-ends", "/").replace(" ", "").split("/");
 
@@ -49,12 +97,12 @@ public class CommandeFind extends Commande {
 		switch (pOption) {
 		case 1:
 
-			if(ordre) {
+			if (ordre) {
 				return pName.startsWith(params[1]) && pName.endsWith(params[2]);
-			}else {
+			} else {
 				return pName.startsWith(params[2]) && pName.endsWith(params[1]);
 			}
-			
+
 		case 2:
 
 			return pName.startsWith(this.params[1]);
